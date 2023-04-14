d26adcb2d48f565cae30448ac53c3cf985a998cf
Kushagra Palod <palod.kushagra@gmail.com>
2023-04-14 12:51:23 +0530

    working with score

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ import io,random
 from streamlit_tags import st_tags
 from PIL import Image
 import pymysql
+from Courses import ds_course,web_course,android_course,ios_course,uiux_course
+import pafy
+import plotly.express as px
 
 
 def pdf_reader(file):
@@ -39,6 +42,20 @@ def show_pdf(file_path):
     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
+def course_recommender(course_list):
+    st.subheader("**Courses & Certificates🎓 Recommendations**")
+    c = 0
+    rec_course = []
+    no_of_reco = st.slider('Choose Number of Course Recommendations:', 1, 10, 4)
+    random.shuffle(course_list)
+    for c_name, c_link in course_list:
+        c += 1
+        st.markdown(f"({c}) [{c_name}]({c_link})")
+        rec_course.append(c_name)
+        if c == no_of_reco:
+            break
+    return rec_course    
+
 connection = pymysql.connect(host='localhost',user='root',password='',db='HireHive')
 cursor = connection.cursor()
 
@@ -62,8 +79,8 @@ def run():
     choice = st.sidebar.selectbox("Choose among the given options:", activities)
     # link = '[©Developed by Spidy20](http://github.com/spidy20)'
     # st.sidebar.markdown(link, unsafe_allow_html=True)
-    img = Image.open('./Logo/logo.svg')
-    img = img.resize((250,250))
+    img = Image.open('./Logo/logo1.png')
+    img = img.resize((400,193))
     st.image(img)
 
     # Create the DB
@@ -87,7 +104,110 @@ def run():
                      PRIMARY KEY (ID));
                     """
     cursor.execute(table_sql)
+    if choice == 'Normal User':
+        # st.markdown('''<h4 style='text-align: left; color: #d73b5c;'>* Upload your resume, and get smart recommendation based on it."</h4>''',
+        #             unsafe_allow_html=True)
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            # with st.spinner('Uploading your Resume....'):
+            #     time.sleep(4)
+            save_image_path = './Uploaded_Resumes/'+pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                ## Get the whole resume data
+                resume_text = pdf_reader(save_image_path)
 
+                st.header("**Resume Analysis**")
+                st.success("Hello "+ resume_data['name'])
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: '+resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: '+str(resume_data['no_of_pages']))
+                except:
+                    pass
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown( '''<h4 style='text-align: left; color: #d73b5c;'>You are looking Fresher.</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >=3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',unsafe_allow_html=True)
 
+                st.subheader("**Skills Recommendation💡**")
+                ## Skill shows
+                keywords = st_tags(label='### Skills that you have',
+                text='See our skills recommendation',
+                    value=resume_data['skills'],key = '1')
+
+                ##  recommendation
+                ds_keyword = ['tensorflow','keras','pytorch','machine learning','deep Learning','flask','streamlit']
+                web_keyword = ['django', 'node jS', 'reactjs', 'php', 'laravel', 'magento', 'wordpress',
+                               'javascript', 'angular js', 'c#', 'flask', 'html5', 'css3', 'tailwind', 'redux', 'mongodb', 'typescript', 'nextjs']
+                android_keyword = ['android','android development','flutter','kotlin','xml','kivy', 'react native']
+                ios_keyword = ['ios','ios development','swift','cocoa','cocoa touch','xcode']
+                uiux_keyword = ['ux','adobe xd','figma','zeplin','balsamiq','ui','prototyping','wireframes','storyframes','adobe photoshop','photoshop','editing','adobe illustrator','illustrator','adobe after effects','after effects','adobe premier pro','premier pro','adobe indesign','indesign','wireframe','solid','grasp','user research','user experience']
+
+                recommended_skills = []
+                reco_field = ''
+                rec_course = ''
+                ## Courses recommendation
+                for i in resume_data['skills']:
+                    ## Data science recommendation
+                    if i.lower() in ds_keyword:
+                        print(i.lower())
+                        reco_field = 'Data Science'
+                        st.success("** Our analysis says you are looking for Data Science Jobs.**")
+                        recommended_skills = ['Data Visualization','Predictive Analysis','Statistical Modeling','Data Mining','Clustering & Classification','Data Analytics','Quantitative Analysis','Web Scraping','ML Algorithms','Keras','Pytorch','Probability','Scikit-learn','Tensorflow',"Flask",'Streamlit']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '2')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Job💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(ds_course)
+                        break
+
+                    ## Web development recommendation
+                    elif i.lower() in web_keyword:
+                        print(i.lower())
+                        reco_field = 'Web Development'
+                        st.success("** Our analysis says you are looking for Web Development Jobs **")
+                        recommended_skills = ['React','Django','Node JS','React JS','php','laravel','Magento','wordpress','Javascript','Angular JS','c#','Flask','SDK']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '3')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Job💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(web_course)
+                        break
+
+                    ## Android App Development
+                    elif i.lower() in android_keyword:
+                        print(i.lower())
+                        reco_field = 'Android Development'
+                        st.success("** Our analysis says you are looking for Android App Development Jobs **")
+                        recommended_skills = ['Android','Android development','Flutter','Kotlin','XML','Java','Kivy','GIT','SDK','SQLite']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '4')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Job💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(android_course)
+                        break
+
+                    ## IOS App Development
+                    elif i.lower() in ios_keyword:
+                        print(i.lower())
+                        reco_field = 'IOS Development'
+                        st.success("** Our analysis says you are looking for IOS App Development Jobs **")
+                        recommended_skills = ['IOS','IOS Development','Swift','Cocoa','Cocoa Touch','Xcode','Objective-C','SQLite','Plist','StoreKit',"UI-Kit",'AV Foundation','Auto-Layout']
+                        recommended_keywords = st_tags(label='### Recommended skills for you.',
+                        text='Recommended skills generated from System',value=recommended_skills,key = '5')
+                        st.markdown('''<h4 style='text-align: left; color: #1ed760;'>Adding this skills to resume will boost🚀 the chances of getting a Job💼</h4>''',unsafe_allow_html=True)
+                        rec_course = course_recommender(ios_course)
+                        break
+
+run()
 
     
